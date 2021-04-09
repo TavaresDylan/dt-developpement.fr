11c6b7a7def44406f80c62e80b7e04122386850b
TavaresDylan <tavares-dylan@bbox.fr>
2021-04-10 00:10:24 +0200

    Menu Burger wip

--- www/public/assets/js/app.js
+++ www/public/assets/js/app.js
@@ -5,12 +5,13 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
+    document.getElementById("hamburger-sidebar").style.top = "0";
   } else {
     document.getElementById("header").style.top = "-450px";
+    document.getElementById("hamburger-sidebar").style.top = "-450px";
   }
   prevScrollpos = currentScrollPos;
 }
-
 
 // Script Menu Hamburger
 var content = document.querySelector('#hamburger-content');
@@ -19,17 +20,48 @@ var sidebar = document.querySelector("#hamburger-sidebar");
 var button = document.querySelector("#hamburger-btn");
 var overlay = document.querySelector("#hamburger-overlay");
 var social = document.querySelector(".social-container")
+var btnCloseSidebar = document.querySelector("#btn-close-sidebar")
 
 // Réplique le contenu du menu dans le tag sidebar-body
 sidebarBody.innerHTML = content.innerHTML;
 sidebarBody.innerHTML += social.innerHTML;
 
+
+function HideShowBurger(mediaWidth) {
+  if (mediaWidth.matches) { // If media query matches
+    console.log("media < 700px")
+    // Make appear burger
+    button.style.display="block";
+    // Make disappear classics nav links and socials
+    social.style.display="none"
+    content.style.display="none"
+  } else {
+    console.log("media > 700px")
+    // Make appear burger
+    button.style.display="none";
+    // Make appear classics nav links and socials
+    social.style.display="block"
+    content.style.display="block"
+  }
+}
+var mediaWidth = window.matchMedia("(max-width: 960px)")
+HideShowBurger(mediaWidth) // Call listener function at run time
+mediaWidth.addListener(HideShowBurger) // Attach listener function on state changes 
+
+
 function showSideBar(){
-  console.log("Click on BurgerButton !")
-  console.log(sidebar)
-  sidebar.style.display="block"
+  if(button.classList[2]== "open"){
+    button.classList.remove("open")
+    sidebar.style.display="none"
+  }else{
+    console.log("Click on BurgerButton !")
+    console.log(sidebar)
+    button.classList.add("open")
+    sidebar.style.display="block"
+  }
 }
 button.addEventListener("click", showSideBar)
+
 
 /* SCRIPT Effet paralax */
 /*$(document).ready(function(){		
