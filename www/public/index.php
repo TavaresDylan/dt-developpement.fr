2596d44887f18641acd4f4bd57c33ed607297b33
TavaresDylan <tavares-dylan@bbox.fr>
2021-05-02 23:00:12 +0200

    rename directories for production

--- www/public/index.php
+++ www/public/index.php
@@ -3,17 +3,14 @@ session_start();
 $maintenance = false;
 //Définition d'une variable qui contiens le chemin de base : /var/www/
 $basePath = dirname(__dir__) . DIRECTORY_SEPARATOR;
-
 // Réquisition du fichier chargeant les dépendances (ex: altorouter , swiftmailer ...ect)
 require $basePath. "vendor/autoload.php";
-
 // Démarre une instance en appelant la méthode du fichier (App)src/app.php
 $app = App\App::getInstance();
 $app->setStartTime();
 $app::load();
-//$app->getMaintenance($maintenance);
-
 // Déclaration des routes
+// Va chercher la fonction getRouter qui se situe dans le fichier src/app.php qui instancie Altorouter
 $app->getRouter($basePath)
 
 // Routes GET
